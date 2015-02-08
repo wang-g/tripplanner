4ef550b43b629bafb847f8a0fe698b06575705d0
jzhong0 <jzhong0@users.noreply.github.com>
2015-02-08 11:28:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -58,14 +58,12 @@ function isOverlapping(start, end){
 });
 
 function removeAll(){
-	console.log("HELLO");
 	$('#calendar').fullCalendar('removeEvents');
 }
 
 var tripArray=[];
 var counter = 0;
 function addDest(e){
-	//e.preventDefault();
 	var loc = $('#loc1').val();
 	var dur = $('#durr').val();
 	if(loc !== "" && dur !== ""){
@@ -75,5 +73,45 @@ function addDest(e){
 		$('#durr').val("");
 		var myDiv = $('<div>').append(loc + " ---- " + dur + " Days");
 		$('#destForm').append(myDiv);
+	}
+}
+
+var key = "ccaac64c-a47d-43a4-8e9f-61faf9296fcd";
+
+var _MS_PER_DAY = 1000 * 60 * 60 * 24;
+var durationAndDates = [];
+// a and b are javascript Date objects
+function dateDiffInDays(b, a) {
+  // Discard the time and time-zone information.
+  var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+function myFunction() {
+
+	var timeBlocks = $('#calendar').fullCalendar('clientEvents');
+
+	for (var i = 0; i < timeBlocks.length; i++) {
+		durationAndDates[i] = {"e": timeBlocks[i], "d": dateDiffInDays(timeBlocks[i]._end._d, timeBlocks[i]._start._d)};
+	}
+
+	durationAndDates.sort(function(a, b) {return a.d - b.d;});
+	tripArray.sort(function(l, d) {return l.dur - d.dur;});
+
+	$('#removeMe').remove();
+	console.log("HELLO 1");
+	window.location = "results.html";
+	console.log("HELLO 2");
+	dispResults();
+}
+
+function dispResults() {
+	console.log("hi");
+	for (var j = 0; j < durationAndDates.length; j++) {
+		var newDestination = $('<div>').append("HELLO");
+		$('#resBody').append(newDestination);
+		console.log("hi");
 	}
 }
